4fc50e81704a226707e4b106e0cbaf75e52e3441
Chase Martin <chasedmartin@gmail.com>
2023-02-11 08:37:56 -0600

    Initial Commit

---
+++ PracticeMultiplication/PracticeMultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeMultiplicationApp.swift
+//  PracticeMultiplication
+//
+//  Created by Chase Martin on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
